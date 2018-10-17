996f0c7b734c56c1aa225376ce80c93df0d7cb3e
goodjwon <jwon76@naver.com>
2018-10-17 17:53:37 +0900

    람다식 표현 추가

--- src/main/java/com/ezezbiz/demo/TestSorting.java
+++ src/main/java/com/ezezbiz/demo/TestSorting.java
@@ -6,6 +6,10 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * 출처 : http://www.mkyong.com/java8/java-8-lambda-comparator-example/
+ */
+
 public class TestSorting {
 
     public static void main(String[] args) {
@@ -26,12 +30,42 @@ public class TestSorting {
         });
 
 
-        System.out.println("\nAfter Sort");
+        System.out.println("\nAfter By age Sort");
         for (Developer developer : developers) {
             System.out.println(developer);
         }
 
+        //userLambda
+        System.out.println("\nLambda age Sort");
+        developers.sort((Developer o1, Developer o2) -> o1.getAge() - o2.getAge());
+        developers.forEach((developer -> System.out.println(developer)));
 
+
+        //sort by name
+        Collections.sort(developers, new Comparator<Developer>() {
+            @Override
+            public int compare(Developer o1, Developer o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        System.out.println("\nAfter By Name Sort");
+        for (Developer developer : developers) {
+            System.out.println(developer);
+        }
+
+        //sort by salary
+        Collections.sort(developers, new Comparator<Developer>() {
+            @Override
+            public int compare(Developer o1, Developer o2) {
+                return o1.getSalary().compareTo(o2.getSalary());
+            }
+        });
+
+        System.out.println("\nAfter By Salary Sort");
+        for (Developer developer : developers) {
+            System.out.println(developer);
+        }
     }
 
     public static List<Developer> getDevelopers(){
