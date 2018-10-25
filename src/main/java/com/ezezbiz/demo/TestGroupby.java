1fc27fafbfff4a5e4318f3fb8c439469455baef3
goodjwon <jwon76@naver.com>
2018-10-25 13:32:52 +0900

    group by sort

--- src/main/java/com/ezezbiz/demo/TestGroupby.java
+++ src/main/java/com/ezezbiz/demo/TestGroupby.java
@@ -1,6 +1,7 @@
 package com.ezezbiz.demo;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -26,6 +27,14 @@ public class TestGroupby {
 
         System.out.println(resultString);
 
+        Map<String, Long> finalMap = new LinkedHashMap<>();
+
+        //Sort a map and add to finalMap
+        result.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue()
+                        .reversed()).forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+
+        System.out.println(finalMap);
 
     }
 }
