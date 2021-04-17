fb056dfd4f933405b0d94fb58438ed63988dd509
JungWon Park <firstman9@protonmail.com>
2021-04-17 14:24:59 +0900

    JPA -findUser Test

---
+++ src/main/java/com/ezezbiz/demo/algorithm/Recurision_1.java
@@ -0,0 +1,16 @@
+package com.ezezbiz.demo.algorithm;
+
+public class Recurision_1 {
+    public static void main(String[] args) {
+        int n = 4;
+        func(n);
+    }
+
+    public static void func(int k) {
+        if (k<=0) return;
+        else {
+            func(k-1);
+            System.out.println("Hello Recursion !!!");
+        }
+    }
+}
