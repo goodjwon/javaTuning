b75b35ba84b2065117c91087b7259718ded273d4
JungWon Park <firstman9@protonmail.com>
2021-05-08 06:49:25 +0900

    sort algorithm

--- src/main/java/com/ezezbiz/demo/algorithm/SelectionSort.java
+++ src/main/java/com/ezezbiz/demo/algorithm/SelectionSort.java
@@ -10,6 +10,8 @@ public class SelectionSort {
      *      1, 2, 3, 4, 8, 6, 7, 10, 5, 9
      *      1, 2, 3, 4, 5, 6, 7, 10, 8, 9 ...
      *      선택후 가장 작은 수를 앞에 놓고 계속 해서 반복한다.
+     *      등차수열 총 55회 계산.
+     *      10 * (10+1)/2 = 55;
      */
     private static int data[] = {5, 10, 1, 7, 8, 6, 4, 2, 3, 9};
 
@@ -18,7 +20,6 @@ public class SelectionSort {
         int min;    // min 최소값
         int index = 0;  // index 가장작은 값의 위치
         int temp;   //temp 값 스위치를 위한 임시 저장소
-
         for(i = 0; i < data.length; i++){
             min = Integer.MAX_VALUE;
             for (j = i; j < data.length; j++){
@@ -37,7 +38,12 @@ public class SelectionSort {
     }
 
     public static void main(String[] args) {
+        long start, end;    //시간 탐색
+        start = System.currentTimeMillis();
         int results[] = sort(data);
+        end = System.currentTimeMillis();
+        System.out.println((end - start) / 1000.0);
+
         for (int a:results){
             System.out.println(a);
         }
