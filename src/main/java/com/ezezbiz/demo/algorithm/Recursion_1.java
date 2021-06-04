cd926542ceb70b7e3d1d9574b058f0233c6e5e66
JungWon Park <firstman9@protonmail.com>
2021-06-05 08:42:56 +0900

    설명추가

--- src/main/java/com/ezezbiz/demo/algorithm/Recursion_1.java
+++ src/main/java/com/ezezbiz/demo/algorithm/Recursion_1.java
@@ -1,5 +1,12 @@
 package com.ezezbiz.demo.algorithm;
 
+
+/**
+ * func(int n)은 음이 아닌 정수 n에 대해서 0에서 n까지의 합을 올바로 계산한다.
+ * 1. n = 0 인경우 0을 반환한다.
+ * 2. 임의의 양의정수 k에 대해서 n < k인 경우 n까지의 합을 올바르게 계산한다고 가정
+ * 3. n=k인 경우 func은 func(k-1) 호출 하는데 2번의 가정에 의해서 0에서 k-1까지의 합이 올바로 계산되어 반환된다.
+ */
 public class Recursion_1 {
     public static void main(String[] args) {
         int n = 4;
